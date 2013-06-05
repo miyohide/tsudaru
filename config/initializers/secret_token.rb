713362b878d55d63149c7b87978e19d2b6cfe261
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-05 22:37:19 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsudaru::Application.config.secret_token = '1be53f7790c504c426afb443a6a1b9a56cd05f846b9a4bcf61d565c1115eda12e34b7881a92c8d826facc7aa28414a7193d040d1cda103df3257ac50aa6260a5'
