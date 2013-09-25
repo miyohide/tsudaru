433ab3a71eff1bc4283ed135158e64c919cfec7c
Hidenori Miyoshi <miyohide@gmail.com>
2013-09-25 21:21:22 +0900

    create new tweet function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,18 @@ class TweetsController < ApplicationController
    end
 
    def create
+      @speaker = params[:tweet_opt][:speaker]
+      @hashtag = params[:tweet_opt][:hashtag]
+      message = "#{@speaker}「" + params[:tweet][:message] + "」 #{@hashtag}"
+
+      @tweet = Tweet.new(message: message)
+
+      if @tweet.save
+         @tweet = Tweet.new
+         render action: 'new'
+      else
+         render action: 'new'
+      end
    end
 
    def index
