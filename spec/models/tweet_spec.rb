90fb19a7c6a228edc33945c67a47373bf8ffad12
Hidenori Miyoshi <miyohide@gmail.com>
2013-09-27 23:26:47 +0900

    add create_full_message spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,8 +8,20 @@ describe Tweet do
 
    describe '.create_full_message' do
       let(:params) { { tweet_opt: { speaker: "speaker", hashtag: "#hashtag" }, tweet: { message: "message" } } }
-      context 'all parameters fill' do
+      context 'all parameters are fill' do
          it { expect(Tweet.create_full_message(params)).to eq "speaker「message」 #hashtag" }
+      end
+
+      context 'speaker is empty' do
+         it { params[:tweet_opt][:speaker] = ''; expect(Tweet.create_full_message(params)).to eq "「message」 #hashtag" }
+      end
+
+      context 'hashtag is empty' do
+         it { params[:tweet_opt][:hashtag] = ''; expect(Tweet.create_full_message(params)).to eq "speaker「message」 " }
+      end
+
+      context 'message is empty' do
+         it { params[:tweet][:message] = ''; expect(Tweet.create_full_message(params)).to eq "speaker「」 #hashtag" }
       end
    end
 end
