790a11c121e2f56737c3ff9c16987b59193ff14c
Hidenori Miyoshi <miyohide@gmail.com>
2013-09-27 23:06:20 +0900

    add create_full_message method spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,7 +2,14 @@
 require 'spec_helper'
 
 describe Tweet do
-  describe 'Validations' do
-    it { should ensure_length_of(:message).is_at_most(140) }
-  end
+   describe 'Validations' do
+      it { should ensure_length_of(:message).is_at_most(140) }
+   end
+
+   describe '.create_full_message' do
+      let(:params) { { tweet_opt: { speaker: "speaker", hashtag: "#hashtag" }, tweet: { message: "message" } } }
+      context 'all parameters fill' do
+         it { expect(Tweet.create_full_message(params)).to eq "speaker「message」 #hashtag" }
+      end
+   end
 end
