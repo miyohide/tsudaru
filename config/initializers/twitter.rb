dff950c9a7a9bf4249c80e817ff51f764504e725
miyohide <miyohide@gmail.com>
2015-11-09 21:12:15 +0900

    delete initial twitter setting. move service layer.

--- config/initializers/twitter.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-Twitter.configure do |config|
-   config.consumer_key       = Setting.twitter_key
-   config.consumer_secret    = Setting.twitter_secret
-   config.oauth_token        = Setting.oauth_token
-   config.oauth_token_secret = Setting.oauth_token_secret
-end
-
