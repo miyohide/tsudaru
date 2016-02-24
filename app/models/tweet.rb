4bf6c3ab3f554c3bdfb0699e674efc3ff795bceb
miyohide <miyohide@gmail.com>
2016-02-24 21:32:35 +0900

    fix indent.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,24 +5,20 @@ class Tweet < ActiveRecord::Base
 
   # 与えられた引数を組み立ててツイートするメッセージを作成する
   def self.create_full_message(params)
-     # Hashtagを表す#は意図的に付与しない
-     "#{params[:tweet_opt][:speaker]}「#{params[:tweet][:message]}」 #{params[:tweet_opt][:hashtag]}"
+    # Hashtagを表す#は意図的に付与しない
+    "#{params[:tweet_opt][:speaker]}「#{params[:tweet][:message]}」 #{params[:tweet_opt][:hashtag]}"
   end
 
   def tweet
-     TwitterAPI.new.client.update(self.message)
-     self.tweeted = true
-     self.save
-  rescue => e
-     if e.instance_of?(Twitter::Error::Forbidden)
-        if e.wrapped_exception.start_with?("Status is a duplicate")
-           self.tweeted = true
-           self.save
-        else
-           raise
-        end
-     else
-        raise
-     end
+    TwitterAPI.new.client.update(self.message)
+    self.tweeted = true
+    self.save
+  rescue Twitter::Error::Forbidden => e
+    if e.wrapped_exception.start_with?("Status is a duplicate")
+      self.tweeted = true
+      self.save
+    else
+     raise
+    end
   end
 end
